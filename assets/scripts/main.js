2a5f4a9f0b9ecad0e4df462e812cbaf96c6538e8
Jessica Lam <jess.lam948@gmail.com>
2021-02-05 23:05:54 -0800

    add basic majority of part 1

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,3 +1,59 @@
-// main.js
+// declaration of elements
+var num_in = document.getElementById("volume-number");
+var slider_in = document.getElementById("volume-slider");
+var aud = document.getElementById("horn-sound");
+var vol_img = document.getElementById("volume-image");
 
-// TODO
+var air_opt = document.getElementById("radio-air-horn");
+var car_opt = document.getElementById("radio-car-horn");
+var party_opt = document.getElementById("radio-party-horn");
+var sound_img = document.getElementById("sound-image");
+
+
+// updates volume and slider value
+num_in.addEventListener("input", function() {
+    aud.volume = Number(num_in.value) / 100;     // update audio volume
+    slider_in.value = num_in.value;     // update slider
+    updateIcon(num_in.value);     // change volume icon
+});
+
+// updates volume and num input value
+slider_in.addEventListener("input", function() {
+    aud.volume = Number(slider_in.value) / 100;     // update audio volume
+    num_in.value = slider_in.value;     // update num input
+    updateIcon(slider_in.value);    // change volume icon
+});
+
+// updates the volume icon based on the sound level
+function updateIcon(vol) {
+    if (vol == 0) {
+        vol_img.src = "./assets/media/icons/volume-level-0.svg";
+    } else if (vol <= 33) {
+        vol_img.src = "./assets/media/icons/volume-level-1.svg";
+    } else if (vol <= 66) {
+        vol_img.src = "./assets/media/icons/volume-level-2.svg";
+    } else {
+        vol_img.src = "./assets/media/icons/volume-level-3.svg";
+    }
+}
+
+air_opt.addEventListener("input", function() {
+    if (air_opt.checked) {
+        sound_img.src = "./assets/media/images/air-horn.svg";
+        aud.src = "./assets/media/audio/air-horn.mp3";
+    }
+});
+
+car_opt.addEventListener("input", function() {
+    if (car_opt.checked) {
+        sound_img.src = "./assets/media/images/car.svg";
+        aud.src = "./assets/media/audio/car-horn.mp3";
+    }
+});
+
+party_opt.addEventListener("input", function() {
+    if (party_opt.checked) {
+        sound_img.src = "./assets/media/images/party-horn.svg";
+        aud.src = "./assets/media/audio/party-horn.mp3";
+    }
+});
