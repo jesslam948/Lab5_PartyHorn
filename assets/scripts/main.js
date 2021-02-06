9a3a095c5d480f18a7977bbaa1c5f99a6244f168
Jessica Lam <jess.lam948@gmail.com>
2021-02-06 00:06:16 -0800

    finish part1

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -9,12 +9,15 @@ var car_opt = document.getElementById("radio-car-horn");
 var party_opt = document.getElementById("radio-party-horn");
 var sound_img = document.getElementById("sound-image");
 
+var honk_btn = document.getElementById("honk-btn");
+honk_btn.type = "button";
 
 // updates volume and slider value
 num_in.addEventListener("input", function() {
     aud.volume = Number(num_in.value) / 100;     // update audio volume
     slider_in.value = num_in.value;     // update slider
     updateIcon(num_in.value);     // change volume icon
+    updateButton(num_in.value);     // update button status
 });
 
 // updates volume and num input value
@@ -22,6 +25,7 @@ slider_in.addEventListener("input", function() {
     aud.volume = Number(slider_in.value) / 100;     // update audio volume
     num_in.value = slider_in.value;     // update num input
     updateIcon(slider_in.value);    // change volume icon
+    updateButton(slider_in.value);     // update button status
 });
 
 // updates the volume icon based on the sound level
@@ -34,6 +38,14 @@ function updateIcon(vol) {
         vol_img.src = "./assets/media/icons/volume-level-2.svg";
     } else {
         vol_img.src = "./assets/media/icons/volume-level-3.svg";
+    }
+}
+
+function updateButton(vol) {
+    if (vol == 0) {
+        honk_btn.disabled = true;
+    } else {
+        honk_btn.disabled = false;
     }
 }
 
@@ -56,4 +68,8 @@ party_opt.addEventListener("input", function() {
         sound_img.src = "./assets/media/images/party-horn.svg";
         aud.src = "./assets/media/audio/party-horn.mp3";
     }
+});
+
+honk_btn.addEventListener("click", function() {
+    aud.play();
 });
